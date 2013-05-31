5e1819f25152139d1831ca6c321a6ab99707e1e4
june <abgood@sina.com>
2013-05-31 14:36:31 +0800

    add sql

---
+++ tsar.sql
@@ -0,0 +1,14 @@
+create table data_collect (
+    `Id` bigint(20) NOT NULL AUTO_INCREMENT primary key COMMENT '编号',
+    ip varchar(20) not null default '',
+    time datetime not null default '2000-00-00 00:00:00',
+    cpu varchar(200) not null default '',
+    mem varchar(200) not null default '',
+    swap varchar(200) not null default '',
+    partition varchar(200) not null default '',
+    tcp varchar(200) not null default '',
+    udp varchar(200) not null default '',
+    traffic varchar(200) not null default '',
+    loadavg varchar(200) not null default '',
+    io varchar(200) not null default ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
